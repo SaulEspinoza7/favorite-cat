907496e8e2e493faed054108e594a5a218134b95
SaulEspinoza7 <ciberelmas4@gmail.com>
2024-02-25 13:49:11 -0500

    25

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import ResultContext from "./context/result-context";
 function App() {
   const [catList, setCatList] = useState([]);
   const [isLoggedIn, setLogged] = useState(false);
-  const [globalUrl, setGlobalUrl] = useState("https://7cba0a41a17a.ngrok.app/")
+  const [globalUrl, setGlobalUrl] = useState("https://7659ce6d62a4.ngrok.app/")
 
   return (
     <React.Fragment>
